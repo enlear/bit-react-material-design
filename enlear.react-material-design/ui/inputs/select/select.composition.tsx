bfa67a2a325314422966e25e541c5ce1b48ce371
Ashan Fernando <ashanf@99x.lk>
2021-04-04 13:29:22 +0530

    Adding select element

--- enlear.react-material-design/ui/inputs/select/select.composition.tsx
+++ enlear.react-material-design/ui/inputs/select/select.composition.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { Select } from './select';
 
 // sets the Component preview in gallery view
+
+const items = [
+  {text: "One", value :"1"},
+  {text: "Two", value :"2"},
+  {text: "Three", value :"3"}
+]
 export const BasicSelect = () => {
-  return <Select>hello from Select</Select>;
+  return <Select label="Age" items = { items}>hello from Select</Select>;
 };
--- enlear.react-material-design/ui/inputs/select/select.tsx
+++ enlear.react-material-design/ui/inputs/select/select.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import MFormControl from '@material-ui/core/FormControl';
+import MMenuItem from '@material-ui/core/MenuItem';
+import MInputLabel from '@material-ui/core/InputLabel';
+import MSelect from '@material-ui/core/Select';
+import { v4 as uuidv4 } from 'uuid';
+
+export interface SelectItem {
+  value: string,
+  text: string
+}
 
 export interface SelectProps extends React.HTMLAttributes<HTMLDivElement> {
+  label: string,
+  items: SelectItem[]
+}
 
-};
-
-export const Select = ( {children, ...rest}: SelectProps ) => {
+export const Select = ({ label, items, ...rest }: SelectProps) => {
+  const id = uuidv4();
   return (
-    <div {...rest}>
-      {children}
-    </div>
+    <MFormControl>
+      <MInputLabel id={`label {id}`}>{label}</MInputLabel>
+      <MSelect labelId={`label {id}`} id={id}>
+        {items.map((item) => (
+          <MMenuItem value={item.value}>{item.text}</MMenuItem>
+        ))}
+      </MSelect>
+    </MFormControl>
   )
 };
