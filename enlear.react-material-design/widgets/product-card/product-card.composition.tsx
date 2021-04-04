85fd6fd6114bf75cce83d572c032ffb1a89b9307
Ashan Fernando <ashanf@99x.lk>
2021-04-05 01:36:36 +0530

    Adding products page

--- enlear.react-material-design/widgets/product-card/product-card.composition.tsx
+++ enlear.react-material-design/widgets/product-card/product-card.composition.tsx
@@ -5,7 +5,7 @@ import { ProductCard } from './product-card';
 export const BasicProductCard = () => {
   return <ProductCard
     title={"Strawberry"}
-    description={"Specially selected strawberries from the worlds best farms"}
+    description={"Fresh strawberries from the worlds best green farms"}
     image={"https://cdn.pixabay.com/photo/2016/04/15/08/04/strawberry-1330459_960_720.jpg"}>
   </ProductCard>
 };
